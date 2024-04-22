74085347e2db00de6ed8d61ebb9b73102706aaf8
Akash Mukherjee <akash3.11.2000@gmail.com>
2024-04-22 18:43:42 +0530

    get all users route defined with pagination and sorting

--- src/main/java/com/example/restappws/ui/controller/UserController.java
+++ src/main/java/com/example/restappws/ui/controller/UserController.java
@@ -6,7 +6,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users")
 public class UserController {
 
-    @GetMapping(path="/{userId}")
+    @GetMapping
+    public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page, @RequestParam(value = "limit", defaultValue = "20") int limit, @RequestParam(value = "sort", defaultValue = "desc", required = false) String sort) {
+        return "get users called with page = " + page + ", limit = " + limit + " and sort = " + sort;
+    }
+
+    @GetMapping(path = "/{userId}")
     public String getUser(@PathVariable String userId) {
         return "get User called with id: " + userId;
     }
