f7c044fdb6a5f02479107de4415a5a13df1da5a1
Akash Mukherjee <akash3.11.2000@gmail.com>
2024-04-22 18:54:50 +0530

    user response object created and returned from getUser route

--- src/main/java/com/example/restappws/ui/controller/UserController.java
+++ src/main/java/com/example/restappws/ui/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.restappws.ui.controller;
 
+import com.example.restappws.ui.model.response.UserRest;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,8 +13,13 @@ public class UserController {
     }
 
     @GetMapping(path = "/{userId}")
-    public String getUser(@PathVariable String userId) {
-        return "get User called with id: " + userId;
+    public UserRest getUser(@PathVariable String userId) {
+        UserRest userData = new UserRest();
+        userData.setFirstName("Akash");
+        userData.setLastName("Mukherjee");
+        userData.setEmail("akash.mukherjee@gmail.com");
+
+        return userData;
     }
 
     @PostMapping
