3488f1be6fb25cc76d3c40e49077c691eacdab9c
Akash Mukherjee <akash3.11.2000@gmail.com>
2024-04-22 18:01:48 +0530

    project initialized using intellij spring initializr

---
+++ src/main/java/com/example/restappws/RestAppWsApplication.java
@@ -0,0 +1,13 @@
+package com.example.restappws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAppWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestAppWsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restappws/RestAppWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restappws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAppWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
