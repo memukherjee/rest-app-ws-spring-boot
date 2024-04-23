dc0caf34d47f8bd01345e763b8d95f4f1ac62af6
Akash Mukherjee <akash3.11.2000@gmail.com>
2024-04-23 11:39:20 +0530

    Post method for user creation is configured with RequestBody and bean validation

--- src/main/java/com/example/restappws/ui/controller/UserController.java
+++ src/main/java/com/example/restappws/ui/controller/UserController.java
@@ -1,8 +1,9 @@
 package com.example.restappws.ui.controller;
 
+import com.example.restappws.ui.model.request.UserDetailsRequestModel;
 import com.example.restappws.ui.model.response.UserRest;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,19 +17,24 @@ public class UserController {
         return "get users called with page = " + page + ", limit = " + limit + " and sort = " + sort;
     }
 
-    @GetMapping(path = "/{userId}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    @GetMapping(path = "/{userId}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<UserRest> getUser(@PathVariable String userId) {
-        UserRest userData = new UserRest();
-        userData.setFirstName("Akash");
-        userData.setLastName("Mukherjee");
-        userData.setEmail("akash.mukherjee@gmail.com");
+        UserRest returnValue = new UserRest();
+        returnValue.setFirstName("Akash");
+        returnValue.setLastName("Mukherjee");
+        returnValue.setEmail("akash.mukherjee@gmail.com");
 
-        return new ResponseEntity<>(userData, HttpStatus.OK);
+        return new ResponseEntity<>(returnValue, HttpStatus.OK);
     }
 
-    @PostMapping
-    public String createUser() {
-        return "create User";
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<UserRest> createUser(@Valid @RequestBody UserDetailsRequestModel userDetails) {
+        UserRest returnValue = new UserRest();
+        returnValue.setFirstName(userDetails.getFirstName());
+        returnValue.setLastName(userDetails.getLastName());
+        returnValue.setEmail(userDetails.getEmail());
+
+        return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
     }
 
     @PutMapping
---
+++ src/main/java/com/example/restappws/ui/model/request/UserDetailsRequestModel.java
@@ -0,0 +1,55 @@
+package com.example.restappws.ui.model.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class UserDetailsRequestModel {
+    @NotNull(message = "First name can not be empty")
+    @Size(min=2, message = "First name must be at least 2 characters long")
+    private String firstName;
+
+    @NotNull(message = "Last name can not be empty")
+    @Size(min=2, message = "Last name must be at least 2 characters long")
+    private String lastName;
+
+    @NotNull(message = "Email can not be empty")
+    @Email(message = "Not a valid email")
+    private String email;
+
+    @NotNull(message = "Password can not be empty")
+    @Size(min=8, max=16, message = "Password must be equal or greater than 8 characters and less than or equal to 16 characters")
+    private String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
