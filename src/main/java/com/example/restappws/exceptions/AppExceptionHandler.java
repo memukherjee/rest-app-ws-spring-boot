539afe71ddd96cfbb826d3dc42ce80918466bde8
Akash Mukherjee <akash3.11.2000@gmail.com>
2024-04-23 22:07:42 +0530

    Exception handler created with custom error message object

---
+++ src/main/java/com/example/restappws/exceptions/AppExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.example.restappws.exceptions;
+
+import com.example.restappws.ui.model.response.ErrorMessage;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class AppExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<ErrorMessage> handleAllExceptions(Exception exception, WebRequest request) {
+        Date currentTimestamp = new Date();
+        String errorMessageDescription = exception.getLocalizedMessage();
+        if(errorMessageDescription != null)
+            errorMessageDescription = exception.toString();
+        ErrorMessage errorMessage = new ErrorMessage(currentTimestamp, errorMessageDescription);
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
