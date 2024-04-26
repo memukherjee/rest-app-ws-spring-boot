7df31189940caea22a7318e38a0f14dabaa58e2a
Akash Mukherjee <akash3.11.2000@gmail.com>
2024-04-26 21:57:05 +0530

    Bean validation errors now producing correct message

--- src/main/java/com/example/restappws/exceptions/AppExceptionHandler.java
+++ src/main/java/com/example/restappws/exceptions/AppExceptionHandler.java
@@ -3,16 +3,27 @@ package com.example.restappws.exceptions;
 import com.example.restappws.ui.model.response.ErrorMessage;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
+import java.util.Objects;
 
 @ControllerAdvice
 public class AppExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Date timestamp = new Date();
+        String errorMessageDescription = Objects.requireNonNull(ex.getBindingResult().getFieldError()).getDefaultMessage();
+        ErrorMessage errorMessage = new ErrorMessage(timestamp, errorMessageDescription);
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<ErrorMessage> handleAllExceptions(Exception exception, WebRequest request) {
         Date currentTimestamp = new Date();
